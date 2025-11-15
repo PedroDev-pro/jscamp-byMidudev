dc53cae45518ea478a693ccf214b8f417eb83e5d
Pedro Campos <ordepprogrammer2003@gmail.com>
2025-11-15 13:13:20 -0300

    agrega lógica para filtrar ofertas de trabajo por tecnología, ubicación y nivel de experiencia; incluye archivo de datos JSON con listados de empleos

--- src/js/searchJobs.js
+++ src/js/searchJobs.js
@@ -9,3 +9,31 @@ jobsListingSection.addEventListener('click', function(event) {
     element.disabled = true
   }
 })
+
+
+//Función que filtra las ofertas de trabajo por tecnología
+const filterTechnology = document.querySelector('#technology');
+
+filterTechnology.addEventListener('change', function (event) {
+    const selectedTechnology = event.target.value;
+    console.log('Selected technology:', selectedTechnology);
+    // Aquí puedes agregar la lógica para filtrar las ofertas de trabajo según la tecnología seleccionada
+});
+
+//Función que filtra las ofertas de trabajo por ubicación
+const filterLocation = document.querySelector('#location');
+
+filterLocation.addEventListener('change', function (event) {
+    const selectedLocation = event.target.value;
+    console.log('Selected location:', selectedLocation);
+    // Aquí puedes agregar la lógica para filtrar las ofertas de trabajo según la tecnología seleccionada
+});
+
+//Función que filtra las ofertas de trabajo por nivel de experiencia
+const experienceLevel = document.querySelector('#experience-level');
+
+experienceLevel.addEventListener('change', function (event) {
+    const selectedExperienceLevel = event.target.value;
+    console.log('Selected expewrience level:', selectedExperienceLevel);
+    // Aquí puedes agregar la lógica para filtrar las ofertas de trabajo según la tecnología seleccionada
+});
